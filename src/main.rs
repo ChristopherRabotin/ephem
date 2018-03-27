399ebc3aa8e92c6a990f92723b60b66e07dec86d
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-03-26 23:26:23 -0600

    Cool! I can read the BSP file and extract the DE number!

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,52 @@
-extern crate nalgebra;
-//extern crate nyx;
-use nalgebra::{Point3, Vector3, Vector6};
-use nalgebra::geometry::{Isometry3, IsometryMatrix3, Quaternion, UnitQuaternion};
-use nalgebra::Real;
+#[macro_use]
+extern crate nom;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+use nom::{digit, IResult};
+
+// Parser definition
+
+use std::str;
+use std::str::FromStr;
+
+// We parse any expr surrounded by parens, ignoring all whitespaces around those
+named!(take_hdr, take_until_and_consume!("DAF/SPK"));
+named!(
+    krnl_name_seek,
+    take_until_and_consume!("JPL") //take_until_and_consume!("JPL planetary and lunar ephmeris")
+);
+named!(take5, take!(5));
+//DE436
+
+#[derive(Debug, PartialEq)]
+struct SPK<'a> {
+    name: &'a [u8],
+}
+
+named!(parser<&[u8],SPK>,
+  do_parse!(    // the parser takes a byte array as input, and returns an A struct
+    take_until_and_consume!("DAF/SPK")       >>      // check for header
+    take_until_and_consume!("JPL planetary and lunar ephmeris ") >> // consume until there
+    dn: take5       >>      // the return value of ret_int1, if it does not fail, will be stored
+    (SPK{name: dn})          // the final tuple will be able to use the variable defined previously
+  )
+);
 
 fn main() {
-    /*let eph = nyx::ephemeris::bsp::from_path("de436.bsp");
-    // Don't lose track of the goal here. It isn't to get the positions and velocities of the planets
-    // but specifically to transform from one frame to another.
-    let earth_now = eph.position(nyx::bodies::Earth, some_time);
-    eph.quaternion*/
-    let o = Point3::new(0.0, 0.0, 0.0);
-    let op = Point3::new(1.0, 1.0, 0.0);
-    let opp = Point3::new(2.0, 2.0, 0.0);
-    let p = Point3::new(2.0, 1.0, 0.0);
-    let p_op = Point3::new(1.0, 0.0, 0.0);
-    let p_opp = Point3::new(0.0, -1.0, 0.0);
-    let op_vec = o - op;
-    let opp_vec = o - opp;
-    let iso_o2op = Isometry3::new(op_vec, nalgebra::zero());
-    let iso_o2opp = Isometry3::new(opp_vec, nalgebra::zero());
-    let iso_direct = Isometry3::new(op - opp, nalgebra::zero());
-    println!("op_vec: {:}", iso_o2op * p);
-    println!("opp_vec: {:}", iso_o2opp * p);
-    println!("direct: {:}", iso_direct * (iso_o2op * p));
+    let mut f = File::open("data/de436.bsp").expect("open");
+    let mut buffer = vec![0; 3000];
+    // read the whole file
+    f.read(&mut buffer).expect("to end");
+    //println!("{:?}", buffer);
+    //let pt = hdr(take7(&buffer).expect("wut?").1).expect("does not start with DAF/SPK");
+    //let pt = take_hdr(&buffer).expect("not an SPK file");
+    //println!("{:?}", pt.0);
+    //let pt = krnl_name_seek(pt.0).expect("oops");
+    //println!("{:?}", pt.0);
+    match parser(&buffer) {
+        Ok(spk_info) => println!("{:?}", str::from_utf8(spk_info.1.name).unwrap()),
+        Err(_) => panic!("oh no"),
+    }
 }
