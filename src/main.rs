4e3a6f7d6bc5b5a3cf58093078cd8c64de6b8cdd
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-05-27 19:05:33 -0600

    Can store the data body data into a HashMap
    Now I need to figure out how to parse the rest of the file

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
 #[macro_use]
 extern crate nom;
-use std::io;
 use std::io::prelude::*;
 use std::fs::File;
-
-use nom::{digit, IResult};
-
-// Parser definition
-
+use std::collections::HashMap;
 use std::str;
-use std::str::FromStr;
 
 named!(consume_until_null, take_until_and_consume!("\0"));
-// We parse any expr surrounded by parens, ignoring all whitespaces around those
-named!(parens<u8>, ws!(delimited!(tag!("("), naif_id, tag!(")"))));
-// named!(
-//     naif_id<u8>,
-//     alt!(map_res!(map_res!(ws!(digit), str::from_utf8), FromStr::from_str) | parens)
-// );
-named!(
-    naif_id<u8>,
-    map_res!(map_res!(ws!(digit), str::from_utf8), FromStr::from_str)
-);
 named!(take_hdr, take_until_and_consume!("DAF/SPK"));
 named!(
     seek_krnl_name,
@@ -35,14 +19,8 @@ named!(
 named!(seek_start_date, take_until_and_consume!("to"));
 named!(seek_bodies, take_until_and_consume!("Bodies included:\0\0"));
 named!(til_next_null, take_until_and_consume!("\0"));
-named!(til_next_dnull, take_until_and_consume!("\0\0"));
 named!(til_next_open_parens, ws!(take_until_and_consume!("(")));
 named!(til_next_close_parens, ws!(take_until_and_consume!(")")));
-named!(
-    naif_id_str,
-    ws!(delimited!(tag!("("), take_until!("("), tag!(")")))
-);
-//named!(body_info, tuple!(body_name, naif_id));
 
 #[derive(Debug, PartialEq)]
 struct SPK<'a> {
@@ -52,15 +30,10 @@ struct SPK<'a> {
     end_date: &'a str,
 }
 
-#[derive(Debug, PartialEq)]
-struct Body<'a> {
-    name: &'a str,
+#[derive(Debug, PartialEq, Clone)]
+struct Body {
+    name: String,
     naif_id: i16, // Some NAIF ID may be negative, esp. for spacecraft
-}
-
-#[derive(Debug)]
-struct GM<'a> {
-    planet_id: &'a str,
     gm: f64,
 }
 
@@ -86,7 +59,7 @@ named!(parse_body_hdr<&[u8],Body>,
     many0!(tag!("\0")) >> // Remove any leading nulls
     name: til_next_open_parens >>
     naif_id : til_next_close_parens >>
-    (Body{name: str::from_utf8(name).unwrap().trim(),
+    (Body{gm: -1.0, name: str::from_utf8(name).unwrap().trim().to_owned(),
         naif_id: str::from_utf8(naif_id).unwrap().parse::<i16>().unwrap()})
   )
 );
@@ -112,14 +85,19 @@ fn main() {
     let mut f = File::open("data/de436.bsp").expect("open");
     let mut buffer = vec![0; 0];
     f.read_to_end(&mut buffer).expect("to end");
+    let mut data: HashMap<i16, Body>;
+    data = HashMap::new();
     match parse_meta(&buffer) {
         Ok(spk_info) => {
             println!("{:?}", spk_info.1);
             match parse_all_body_hdr(spk_info.0) {
                 Ok(body_hdrs) => {
-                    println!("{:?}", (body_hdrs.1).0);
+                    // Let put all this into a HashMap for quick access
+                    for body in &((body_hdrs.1).0) {
+                        data.insert(body.naif_id, body.to_owned());
+                    }
+                    println!("{:?}", data);
                     let mut buf = seek_to_gms(body_hdrs.0).unwrap().0;
-                    let mut gms = Vec::with_capacity(10);
                     loop {
                         match parse_each_gm(buf) {
                             Ok(one) => {
@@ -139,12 +117,50 @@ fn main() {
                                     }
                                 }
                                 if mu > -1.0 {
-                                    let read_gm = GM {
-                                        planet_id: p_id,
-                                        gm: mu,
-                                    };
-                                    println!("{:?}", read_gm);
-                                    gms.push(read_gm);
+                                    // If it's an integer, update the appropriate value.
+                                    match p_id.parse::<i16>() {
+                                        Ok(p_id) => {
+                                            let naif_id =
+                                                if p_id < 4 { p_id * 100 + 99 } else { p_id };
+                                            let mut cur_data =
+                                                data.get(&naif_id).unwrap().to_owned();
+                                            cur_data.gm = mu;
+                                            data.insert(naif_id, cur_data.to_owned());
+                                            if p_id < 3 {
+                                                // Venus exists as both "Venus" and "Venus Barycenter"
+                                                let mut cur_data =
+                                                    data.get(&p_id).unwrap().to_owned();
+                                                cur_data.gm = mu;
+                                                data.insert(p_id, cur_data.to_owned());
+                                            }
+                                        }
+                                        Err(_) => {
+                                            // This ID has a name.
+                                            let naif_id = match p_id {
+                                                "S" => {
+                                                    10 // Sun
+                                                }
+                                                "M" => {
+                                                    // Moon
+                                                    301
+                                                }
+                                                "B" => {
+                                                    // Earth barycenter
+                                                    3
+                                                }
+                                                _ => {
+                                                    println!("unknown body `GM{}`", p_id);
+                                                    -1
+                                                }
+                                            };
+                                            if naif_id > -1 {
+                                                let mut cur_data =
+                                                    data.get(&naif_id).unwrap().to_owned();
+                                                cur_data.gm = mu;
+                                                data.insert(naif_id, cur_data.to_owned());
+                                            }
+                                        }
+                                    }
                                 } else {
                                     break;
                                 }
@@ -156,10 +172,11 @@ fn main() {
                             }
                         }
                     }
+                    println!("\n{:?}", data);
                 }
                 Err(_) => panic!("failed"),
             }
         }
-        Err(err) => panic!("oh no: {}"),
+        Err(_) => panic!("oh no: {}"),
     }
 }
